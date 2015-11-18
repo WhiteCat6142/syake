37725df1d683b70472f7c104c640ce755acce1d1
WhiteCat6142 <whitecat6142+git@gmail.com>
2015-11-18 13:19:49 +0900

    v0.2

--- hello.js
+++ hello.js
@@ -12,14 +12,18 @@ if(!req.ip.match(config.vistor)){next();}else{res.sendStatus(403);}
 
 //app.use(compression());
 
-app.use(express.static('www'));
+app.use("/gateway.cgi",express.static('www',{maxAge:600}));
 
 app.set('view engine', 'jade');
 app.set('views', './views');
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 var router = require('./syake/router');
 app.use('/',router);
 
+app.use(function(err, req, res, next){res.status(500).send(err.name + ": " + err.message);});
 app.use(function(req, res, next){res.sendStatus(404);});
 
 http.createServer(app).listen(3000, function(){
--- syake/router.js
+++ syake/router.js
@@ -1,70 +1,12 @@
 var express = require('express');
 var app = express.Router();
 
-var url = require('url');
-var qs = require('querystring');
-
-var http = require('http');
-
-var a = [{link:"/a",title:"a",num:1}];
-var tags =["a","b","c"];
-app.get('/', function(req, res) {
-  res.render('index',{threads:a,tags:tags});
-});
-
-var db = require('./sql');
+var db = require('./gateway');
 app.get('/',db.index);
-app.post('/test/bbs.cgi',db.index);
+app.get('/gateway.cgi',db.index);
+app.get('/gateway.cgi/changes',db.changes);
+app.get('/thread.cgi/:id',db.thread);
+app.post('/thread.cgi',db.post);
+app.post('/test/bbs.cgi',db.post);
 
 module.exports = app;
-/*
-chat=["test"];
-
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-app.get('/calc', function(req, res) {
-  var x = url.parse(req.url, true);
-  var a = x.query["a"];
-  var b = x.query["b"];
-  res.render('calc',{result:(a * b)});
-});
-
-app.get('/bbs', function(req, res) {
-   res.render('bbs',{ip:chat.toString()});
-});
-
-app.post('/bbs', function(req, res) {
-req.data = "";
-req.on("readable", function () {if(tmp=req.read())req.data += tmp;});
-req.on("end", function () {
-var query = qs.parse(req.data);
-chat.push(query.content);
-res.redirect('/bbs');
-});
-});
-
-app.get('/rss', function(req, res) {
-var url = 'http://rss.rssad.jp/rss/forest/rss.xml';
-res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
-r=res;
-var response = http.get(url, function(res){
-    var body = '';
-
-    res.on('data', function(chunk){
-        body += chunk;
-    });
- 
-    res.on('end', function(res){
-        //ret = JSON.parse(body);
-        response = body;
-r.write(body);
-r.end();
-    });
-}).on('error', function(e){
-    console.log(e.message); //ÉGÉâÅ[éû
-});
-});
-
-*/
