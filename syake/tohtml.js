c9d4d666c12b632e1dcecbdb85a35aa463d2a78e
WhiteCat6142 <whitecat6142+git@gmail.com>
2016-04-18 10:19:09 +0900

    v0.40

--- syake/tohtml.js
+++ syake/tohtml.js
@@ -2,7 +2,7 @@ const image=["png","bmp","gif","jpg","jpeg","bin","tif","tiff"]
 
 
 exports.t=function(body) {
-        var b3 = body.match(/h?ttps?:\/\/[0-9-_a-zA-Z.\/!#$&+,:;=@\[\]\?]+/g);
+        var b3 = body.match(/h?ttps?:\/\/[0-9-_a-zA-Z.\/%!#$&+,:;=@\[\]\?]+/g);
         if (b3) {
           b3.forEach(function (ele) {
             var link = (ele[0] == 'h') ? ele : 'h' + ele;
