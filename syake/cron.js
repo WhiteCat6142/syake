3180a9690d69d5bf565ba75eaadd8869614344be
WhiteCat6142 <whitecat6142+git@gmail.com>
2016-04-18 15:00:43 +0900

    h7.2

--- syake/cron.js
+++ syake/cron.js
@@ -99,7 +99,7 @@ function get(url){
 		const o=Url.parse(url);
 		o.agent=ag;
 		const request=http.request(o, function(res){
-            if(res.statusCode!=200)reject(res.statusCode);
+            if(res.statusCode!=200)reject(res);
 				var bufs = []; 
 				res.on('data', function(chunk){bufs.push(chunk);});
 				res.on('end', function(){
@@ -178,6 +178,6 @@ if(api.config.range.first){
 for(var i=0;i<3;i++){
 	readLine(nodeUrl("ygg.io/server","node"),function(node) {
 		api.config.friends.push(node);
-		readLine(nodeUrl(node,"join")+"/:"+api.port+"+server.cgi");
+		readLine(nodeUrl(node,"join")+"/morning-mountain-32572.herokuapp.com:80+server.cgi");
 	});
 }
