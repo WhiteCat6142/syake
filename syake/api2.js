87560760f162fefde097302f6277d123fad5a922
WhiteCat6142 <whitecat6142+git@gmail.com>
2016-04-26 17:48:12 +0900

    h10.2

--- syake/api2.js
+++ syake/api2.js
@@ -168,11 +168,11 @@ exports.post = function (file, name, mail, body, time, subject) {
 const aday=60*60*24;
 function add(file,stamp,id,content){
     knex.transaction(function(trx) {
-        console.log(trx.select("stamp","id").from(ff(file)).whereBetween("stamp",[stamp-aday,stamp+aday]).toString());
+        console.log(trx.select("stamp","id").from(ff(file)).whereBetween("stamp",[stamp-aday,stamp+aday]).andWhere("id",id).toString());
         return trx.select("stamp","id").from(ff(file)).whereBetween("stamp",[stamp-aday,stamp+aday]).andWhere("id",id)
         .then(function(rows){
             if (rows.length > 0) {
-                if(stamp!==rows[0].stamp)return knex("spam").insert({id:id});
+                if(stamp!==rows[0].stamp)console.log("duplicate post!");
                 return;
             }
             exports.update.emit('update', file, stamp, id, content);
