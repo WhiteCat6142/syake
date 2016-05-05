ad629c42e2df708de41acb95a87cd56fa813bdde
WhiteCat6142 <whitecat6142+git@gmail.com>
2016-05-05 12:30:33 +0900

    v0.61

--- syake/apollo.js
+++ syake/apollo.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var crypto = require('crypto');
+var bigint = require('bigint-node');
 
 var primes = [];
 function check(n){
@@ -24,7 +25,7 @@ function pow(base, exponent, modulus) {
       result = (result * base) % modulus;
     }
     base = (base * base) % modulus;
-    exponent = Math.floor(exponent / 2);
+    exponent = exponent>>1;
   }
 return (result);
 }
@@ -83,16 +84,51 @@ while(i--)md5=md5+'\0';
     return m==x;
 }
 
-/*
+
 function varify(target,sign,pubkey){
-const verify = crypto.createVerify('rsa-sha256');
-var md5 = crypto.createHash('md5').update(target, 'utf8').digest('binary');
-md5=md5.slice(0,63);
-verify.write(md5);
+    for(var result="", lines=0;result.length-lines < pubkey.length;lines++) {
+        result+=pubkey.substr(result.length-lines,64)+"\n"
+    }
+    pubkey="-----BEGIN PUBLIC KEY-----\n"+result+"-----END PUBLIC KEY-----";
+    console.log(pubkey);
+const verify = crypto.createVerify('RSA-MD5');
+//console.log(crypto.getCiphers());
+//var md5 = crypto.createHash('md5').update(target, 'utf8').digest('hex');
+//console.log(md5);
+//md5=md5.slice(0,63);
+verify.write(target);
 verify.end();
-console.log(sign.verify(pubkey, sign,"base64"));
-};
+//console.log(verify.verify(pubkey, sign,"base64"));
+//var decipher = crypto.createDecipher('rsa', pubkey);
+//decipher.update(sign, 'hex', 'utf8');
+//var dec = decipher.final('utf8');
+//console.log(dec);
+/*
+var n=bigint.ParseFromString(new Buffer(pubkey,"base64").toString("hex"),16);
+var c=bigint.ParseFromString(new Buffer(sign,"base64").toString("hex"),16);
+var e=bigint.FromInt(65537);
+console.log(n.powMod);
+var m=bigint.powMod(c,e,n);
+console.log(bigint.bigInt2str(c,16))
 */
+pubkey="-----BEGIN PUBLIC KEY-----\n\
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkRWAQ0O9LgBoHNAB5m1X\n\
+8e1sPTzKmBTPCFTSRTzw0AjZozIbN4nIp/3jnQHTbcY0Bf5MDWmtdheSK1a+ew34\n\
+YcgN2b9Shr+3yZv9PJ97i7gRCqOnI7jbm7PXFBNw1I4aMYc6tV7TKFzvx6008/nY\n\
+vN3Jey6Z8ItS/FLQRDkV9m/WQkhJpYgvmD6qiwj9d+un+moBQ5/PPgn7Qkg5GyxZ\n\
+Uy9PsblUDSrIA0bEiv/wQOXCYUvL9OFzxTUSeIHpdGibhPQVxX3Jnpr293Iq/mOK\n\
+n3ZO+xBID26m3L8+ik64wte041y1S4HHaE9Q082ai/uBduAwIHcJY5VAHborZYCS\n\
+aQIDAQAB\n\
+-----END PUBLIC KEY-----"
+console.log(pubkey);
+var dec = crypto.publicDecrypt({key:pubkey,padding:crypto.RSA_NO_PADDING},new Buffer(sign,"base64"));
+console.log(dec)
+};
+
+a="NzA2NDEyOSw5MTExMjcwLDQ4MjA0NTcsMjY1ODc4MSw5ODA5NDA4LDI4NTI3OTIsNDQ4NTczNCwyMzUxNDQ2LDcxNjM4MDgsOTI1MDA3NywyODYwNzUsNTgxODYzNyw3Mzg2NjY1LDM4MjA0MDIsNzg2NTkzNiw4NDI5MTYwLDY5NjkzMzYsNzAwNTA1NCw0NDEyNDU5LDU1MTc1OTMsNDQ1Njc4LDE5MTE5"
+b="NjA2NzQyMSwyMjQ3ODQsNjg4MDU1OCw4Nzk5NjQ1LDgzODk2MjQsNDQ5NTEyOCw2MDI4NTY0LDg3NDc4NDMsODY1NjU0NSw4NDExNDA5LDg0MDAwNjgsNDE4MTkxNiwzODIzNDYsNzUzMzgwOSwyMzA1MTEyLDMwNjE0MSw3ODk4NDc5LDkzMzI3MjIsOTA4MDMyOCw3MzgzMjE5LDU4NDI2OTgsMzE5MTU1"
+console.log(new Buffer(a,base64).)
+
 var result1 = varify("body:&gt;&gt;f9f5dae8<br>文字列とリストは同じように扱えるからね。<br>文字列は変更不可オブジェクトで、リストは変更可能オブジェクトという違いはあるが。<>name:白帽子",
     "JLYlqbhL1hO0mIpnYIRvz7FyerE+dIT/aP3UuAVQEIiix5tJBmg7y4WUKEB1EcCzuWbz0eCE37Ax7o8HeyOwLA",
     "7iIuGXJ/jFbB8yfUzISydn2vhi33O8ZnPNJBvQKiDxo60RWUgSPk+Ch2X82EVIGT2GmMF1FPDXCMS1wwWlQzRA"
@@ -102,3 +138,17 @@ var result2 = varify("body:test",
 "DpmzfQSOhbpxE7xuaiEao3ztv9NAJi/loTs2N43f5hC3XpT3z9VhApcrYy94XhMBKONo5H14c8STrriPJnCcVA"
 );
 console.log(result1,result2);
+
+//1462245355<>5de48357846c2e88967e6ea10543aa0f<>
+//
+//<>pubkey:
+//key=test
+//<>sign:
+//<>target:body,stamp,file_name
+
+function test(){
+    var key="DpmzfQSOhbpxE7xuaiEao3ztv9NAJi/loTs2N43f5hC3XpT3z9VhApcrYy94XhMBKONo5H14c8STrriPJnCcVA";
+    var sign="peTxFudKnbhL5ntBeNhn3Q+YQfMQTJ1BgCnMz8lRHCiet80+5yLmqJfEaWFclabdVRPiaqmGADw64wnNDXWDTA";
+    var body="body:test<>stamp:1462245355<>file_name:thread_E38386E382B9E38388";
+    
+}
--- www/js/bbs.js
+++ www/js/bbs.js
@@ -28,13 +28,81 @@ window.onload = function () { document.getElementById("cmd").value = "post"; };
   window.addEventListener('scroll', listen);
   listen();
 })();
-      /*
+
 function genkey(k) {
-  var cry=window.crypto.subtle;
   var pq=md5(k)+md5(k+"pad1")+md5(k+"pad2")+md5(k+"pad3");
-  var p=pq.substr(0,7),q=pq.substr(7,9);
-  var e=0x100001;
-  p=p|Math.pow(2,215);q=q|Math.pow(2,279);
-  base
+  console.log(pq.length);
+  var p=s(pq.substring(0,28)),q=s(pq.substring(28,64));
+  var e=bigInt(65537);
+  var test=bigInt(0x7743);
+  var keyd=null,keyn=null;
+  p=p.or(Math.pow(2,215));q=q.or(Math.pow(2,279));
+  for(var i=0;i<300;i++){
+    p=primize(p);
+    q=primize(q);
+    var phi=p.prev().times(q.prev());
+    keyd=modinv(e,phi);
+    if(keyd.isZero())continue;
+    keyn=p.times(q);
+    var t=test.modPow(e,keyn).modPow(keyd,keyn);
+    if(t.eq(test))break;
+    p=p.plus(2);q=q.plus(2);
+  }
+  console.log(toB(keyd));//sec
+  console.log(toB(keyn));//pub
 };
-*/
+function primize(i) {
+  if(i.isEven())i=i.next();
+  while(!i.isProbablePrime(8))i=i.plus(2);
+  return i;
+}
+function modinv(a,n) {
+  var s=a,t=n,u=bigInt.one,v=bigInt.zero;
+  while(s.isPositive()){
+    q=t.divide(s);
+    var w1=t.subtract(q.times(s));
+    t=s;
+    s=w1;
+    var w2=v.subtract(q.times(u));
+    v=u;
+    u=w2;
+  }
+  return v.add(n).mod(n);
+}
+var en="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+function toB(x){
+  var r="";
+  while(x.isPositive()){
+    r+=en[x.mod(64).toJSNumber()];
+    x=x.shiftRight(6);
+  }
+  return r;
+  }
+  
+  
+  
+  
+function fromS(str){
+    var tmp =bigInt.zero;
+    var i = str.length;
+    while(i--){
+        tmp=tmp.shiftLeft(8).add(str.charCodeAt(i));
+    }
+    return tmp;
+}
+	var dic = new Object();
+	dic[0x41]= 0; dic[0x42]= 1; dic[0x43]= 2; dic[0x44]= 3; dic[0x45]= 4; dic[0x46]= 5; dic[0x47]= 6; dic[0x48]= 7; dic[0x49]= 8; dic[0x4a]= 9; dic[0x4b]=10; dic[0x4c]=11; dic[0x4d]=12; dic[0x4e]=13; dic[0x4f]=14; dic[0x50]=15;
+	dic[0x51]=16; dic[0x52]=17; dic[0x53]=18; dic[0x54]=19; dic[0x55]=20; dic[0x56]=21; dic[0x57]=22; dic[0x58]=23; dic[0x59]=24; dic[0x5a]=25; dic[0x61]=26; dic[0x62]=27; dic[0x63]=28; dic[0x64]=29; dic[0x65]=30; dic[0x66]=31;
+	dic[0x67]=32; dic[0x68]=33; dic[0x69]=34; dic[0x6a]=35; dic[0x6b]=36; dic[0x6c]=37; dic[0x6d]=38; dic[0x6e]=39; dic[0x6f]=40; dic[0x70]=41; dic[0x71]=42; dic[0x72]=43; dic[0x73]=44; dic[0x74]=45; dic[0x75]=46; dic[0x76]=47;
+	dic[0x77]=48; dic[0x78]=49; dic[0x79]=50; dic[0x7a]=51; dic[0x30]=52; dic[0x31]=53; dic[0x32]=54; dic[0x33]=55; dic[0x34]=56; dic[0x35]=57; dic[0x36]=58; dic[0x37]=59; dic[0x38]=60; dic[0x39]=61; dic[0x2b]=62; dic[0x2f]=63;
+function fromB(str){
+	var bin = bigInt.zero;
+	var i = str.length;
+	while(i--){
+    bin=bin.shiftLeft(6).add(dic[str.charCodeAt(i)]);
+	}
+	return bin;
+}
+function varify(m,sign,pubkey){
+return fromS(md5(m)).eq(fromB(sign).modPow(65537,fromB(pubkey)));
+}
